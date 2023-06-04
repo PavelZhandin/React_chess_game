832a0909a4ff234718967f00177968cc31589a76
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-06-04 21:45:05 +0300

    draw chess board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
 import { Board } from "./models/Board";
 
 function App() {
   const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
   return (
     <div className="App">
-      <BoardComponent />
+      <BoardComponent board={board} setBoard={setBoard} />
     </div>
   );
 }
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Cell } from "../models/Cell";
 
-const CellComponent = () => {
-  return <div className="cell">CellComponent</div>;
+interface CellProps {
+  cell: Cell;
+}
+
+const CellComponent = ({ cell }: CellProps) => {
+  return <div className={["cell", cell.color].join(" ")}></div>;
 };
 
 export default CellComponent;
