44e6bb6857e9f157614bd72c4606bbe7c9dddc0e
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-06-05 17:14:16 +0300

    render figures on board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,12 +1,24 @@
-import React from "react";
 import { Cell } from "../models/Cell";
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-const CellComponent = ({ cell }: CellProps) => {
-  return <div className={["cell", cell.color].join(" ")}></div>;
+const CellComponent = ({ cell, selected, click }: CellProps) => {
+  return (
+    <div
+      className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+      onClick={() => click(cell)}
+      style={{ background: cell.available && cell.figure ? "green" : "" }}
+    >
+      {cell.available && !cell.figure && <div className="available" />}
+      {cell.figure && (
+        <img draggable={false} src={cell.figure?.logo as string} alt="" />
+      )}
+    </div>
+  );
 };
 
 export default CellComponent;
